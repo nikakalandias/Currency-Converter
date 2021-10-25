da7acf00f56843a174e385f4ef56a7e0a77e29b0
nika <nikakalandias@gmail.com>
2021-10-26 01:40:33 +0300

    First Commit

---
+++ currency-Converter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  currency-Converter
+//
+//  Created by Nika Kalandia on 25.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func getRates(_ sender: Any) {
+    }
+    
+}
+
