fa8735018d2c9df7911c7f47104c9e1311c7a9ff
nika <nikakalandias@gmail.com>
2021-10-26 15:35:27 +0300

    Added; URL, Session, Task

--- currency-Converter/ViewController.swift
+++ currency-Converter/ViewController.swift
@@ -21,7 +21,41 @@ class ViewController: UIViewController {
     }
 
     @IBAction func getRates(_ sender: Any) {
+        
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=2227346145ef66e7040914490b41c6cb")
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url!) { Data, Response, Error in
+            if Error != nil {
+                let alert = UIAlertController(title: "ERROR!", message: Error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "Okey", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+                
+            }else {
+                
+                if Data != nil {
+                    
+                    do {
+                       let jsonResponse = try JSONSerialization.jsonObject(with: Data!, options: JSONSerialization.ReadingOptions.mutableContainers)
+                        
+                        DispatchQueue.main.async {
+                            print(jsonResponse)
+                            
+                        }
+                    }catch {
+                        print("ERROR!")
+                    }
+                }
+            }
+            
+        }
+        
+        task.resume()
+        
     }
+    
     
 }
 
